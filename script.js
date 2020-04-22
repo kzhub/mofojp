04756b15a688bb8065e132ba5e47e90553b18d60
kzhub <55772750+kzhub@users.noreply.github.com>
2020-04-22 21:02:34 +0900

    a

---
+++ script.js
@@ -0,0 +1,23 @@
+function pen(){
+  let modal= document.getElementById("modal");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+
+}
+
+function pen2(){
+  let modal= document.getElementById("modal");
+  modal.style.display = 'none';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'none';
+
+}
+
+function pen3(){
+  let modal= document.getElementById("modal");
+  modal.style.display = 'none';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'none';
+
+}
