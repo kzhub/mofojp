b57222ccf767cac79b2eee3eaf65b2377a510b29
kzhub <55772750+kzhub@users.noreply.github.com>
2020-06-23 22:34:10 +0900

    change

--- script.js
+++ script.js
@@ -3,26 +3,40 @@ function pen(){
   modal.style.display = 'block';
   let mask= document.getElementById("mask");
   mask.style.display = 'block';
-
 }
-
 function pen2(){
-  let modal= document.getElementById("modal");
-  modal.style.display = 'none';
+  let modal= document.getElementById("modatea");
+  modal.style.display = 'block';
   let mask= document.getElementById("mask");
-  mask.style.display = 'none';
-
+  mask.style.display = 'block';
 }
+function pen4(){
+  let modal= document.getElementById("modal3");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+function pen5(){
+  let modal= document.getElementById("modal4");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+
+
+
 
 function pen3(){
   let modal= document.getElementById("modal");
   modal.style.display = 'none';
+  let modala= document.getElementById("modal4");
+  modala.style.display = 'none';
+  let modalb= document.getElementById("modal3");
+  modalb.style.display = 'none';
+  let modalc= document.getElementById("modatea");
+  modalc.style.display = 'none';
   let mask= document.getElementById("mask");
   mask.style.display = 'none';
 
-}
-
-function hide(){
-  let ab= document.getElementById('kesu');
-  ab.style.display = 'none';
 }
